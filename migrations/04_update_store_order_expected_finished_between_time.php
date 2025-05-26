d371b64c90711477744c0bae2719c298d4c2b464
david <367013672@qq.com>
2025-05-26 14:48:10 +0800

    新增：预期送达时间

--- migrations/04_update_store_order_expected_finished_between_time.php
+++ migrations/04_update_store_order_expected_finished_between_time.php
@@ -1,6 +1,7 @@
 <?php
 
 use Phinx\Db\Adapter\AdapterInterface;
+use Phinx\Db\Adapter\MysqlAdapter;
 use think\migration\Migrator;
 use think\migration\db\Column;
 
@@ -34,9 +35,10 @@ class UpdateStoreOrderExpectedFinishedBetweenTime extends Migrator
     public function change()
     {
         $table = $this->table("store_order");
-        $table->addColumn('expected_finished_start_time', AdapterInterface::PHINX_TYPE_DATETIME, ['comment' => '预期送达开始时间', 'null' => true])
+        $table->addColumn('expected_finished_time', AdapterInterface::PHINX_TYPE_INTEGER, ['comment' => '预期送达时间', 'null' => false, 'default' => 0, 'signed' => false])
+            ->addColumn('expected_finished_start_time', AdapterInterface::PHINX_TYPE_DATETIME, ['comment' => '预期送达开始时间', 'null' => true])
             ->addColumn('expected_finished_end_time', AdapterInterface::PHINX_TYPE_DATETIME, ['comment' => '预期送达结束时间', 'null' => true])
-            ->addIndex('expected_finished_start_time')
+            ->addIndex('expected_finished_time')
             ->update();
     }
 }
